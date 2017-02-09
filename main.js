a4aa9aca3a80a8bd919ba4b1d148d89fcb45ba1d
assafzarfaty <zarfaty@blipitapp.com>
2017-02-09 13:27:29 +0200

    full solutions including extensions to the following exercise- http://elevation-learn.herokuapp.com/#/lesson/588dc2e3296f2c001c7a00c6

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+  'Bob': { 
+  	claimed: false },
+  'Ted': { 
+  	claimed: true }
+}
+		
+function claimReservation(name) {
+	var lowerName = name.slice(1,(name.length)).toLowerCase();
+	var validName = name.slice(0,1).toUpperCase() + lowerName;
+	console.log(validName);
+
+	if (reservations[validName]){
+		if (reservations[validName].claimed){
+			alert("Welcome to the store");
+		}
+		else {
+			alert("you have an order to be claimed");
+		}
+	}
+	else{
+		reservations[validName] = {claimed : true} ;
+		alert("New guest added to a table");
+	}
+	return reservations;
 }
 
-var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
-  // write your code here!
-}
 
-claimReservation();
+
+
